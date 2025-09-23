4756666c986766afd4197c706089c8ee49ca09d4
Wojciech Pilzak <wojciechpilzak@gmail.com>
2025-09-23 22:09:31 +0100

    [DWP-02] feature: TicketRequestValidator implementation test: TicketRequestValidatorTest more test scenarios

--- src/main/java/uk/gov/dwp/uc/pairtest/service/validator/TicketRequestValidator.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/service/validator/TicketRequestValidator.java
@@ -1,10 +1,41 @@
 package uk.gov.dwp.uc.pairtest.service.validator;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketRequestValidator {
 
     public void validate(Long accountId, TicketTypeRequest[] requests) {
+        if (accountId == null || accountId <= 0) {
+            throw new InvalidPurchaseException();
+        }
 
+        int totalTickets = 0;
+        int adultTickets = 0;
+        int childTickets = 0;
+        int infantTickets = 0;
+
+        for (TicketTypeRequest request : requests) {
+            if (request.noOfTickets() < 0) {
+                throw new InvalidPurchaseException();
+            }
+            totalTickets += request.noOfTickets();
+            if (totalTickets > 25) {
+                throw new InvalidPurchaseException();
+            }
+            switch (request.type()) {
+                case ADULT -> adultTickets += request.noOfTickets();
+                case CHILD -> childTickets += request.noOfTickets();
+                case INFANT -> infantTickets += request.noOfTickets();
+            }
+        }
+
+        if (totalTickets == 0 || totalTickets > 25) {
+            throw new InvalidPurchaseException();
+        }
+
+        if ((childTickets > 0 || infantTickets > 0) && adultTickets==0) {
+            throw new InvalidPurchaseException();
+        }
     }
 }
--- src/test/java/uk/gov/dwp/uc/pairtest/service/validator/TicketRequestValidatorTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/service/validator/TicketRequestValidatorTest.java
@@ -1,8 +1,12 @@
 package uk.gov.dwp.uc.pairtest.service.validator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.*;
@@ -34,12 +38,48 @@ class TicketRequestValidatorTest {
     }
 
     @Test
-    void shouldThrowWhenAccountIdInvalid() {
+    void shouldThrowWhenMoreThan25TicketsForTwoDifferentTicketTypes() {
+        TicketTypeRequest adult = new TicketTypeRequest(ADULT, 23);
+        TicketTypeRequest child = new TicketTypeRequest(CHILD, 12);
+        assertThrows(InvalidPurchaseException.class,
+                () -> validator.validate(1L, new TicketTypeRequest[]{adult, child}));
+    }
+    @Test
+    void shouldThrowWhenAccountIdIsZero() {
         TicketTypeRequest adult = new TicketTypeRequest(ADULT, 1);
         assertThrows(InvalidPurchaseException.class,
                 () -> validator.validate(0L, new TicketTypeRequest[]{adult}));
     }
 
+    @Test
+    void shouldThrowWhenAccountIdIsNull() {
+        TicketTypeRequest adult = new TicketTypeRequest(ADULT, 1);
+        assertThrows(InvalidPurchaseException.class,
+                () -> validator.validate(null, new TicketTypeRequest[]{adult}));
+    }
+
+    static Stream<TicketTypeRequest> negativeRequests() {
+        return Stream.of(
+                new TicketTypeRequest(ADULT, -1),
+                new TicketTypeRequest(CHILD, -2),
+                new TicketTypeRequest(INFANT, -3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeRequests")
+    void shouldThrowWhenNegativeTicketCount(TicketTypeRequest bad) {
+        assertThrows(InvalidPurchaseException.class,
+                () -> validator.validate(1L, new TicketTypeRequest[]{bad}));
+    }
+
+    @Test
+    void shouldThrowWhenAnyEntryNegativeEvenIfOthersValid() {
+        TicketTypeRequest adultOk = new TicketTypeRequest(ADULT, 2);
+        TicketTypeRequest childBad = new TicketTypeRequest(CHILD, -1);
+        assertThrows(InvalidPurchaseException.class,
+                () -> validator.validate(1L, new TicketTypeRequest[]{adultOk, childBad}));
+    }
 
     //positive ways tests
     //We don't need too many positive path tests
